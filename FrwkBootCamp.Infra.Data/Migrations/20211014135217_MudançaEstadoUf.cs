3b501fd0d79bfb6a1841d3457a9e3b458c69f3f6
Roberta Rodrigues <robertasrodriguesalves@gmail.com>
2021-10-14 11:07:26 -0300

    Remoção ignore do migrations

---
+++ FrwkBootCamp.Infra.Data/Migrations/20211014135217_MudançaEstadoUf.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FrameBook.Infra.Data.Migrations
+{
+    public partial class MudançaEstadoUf : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Estado",
+                table: "Profissional");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Uf",
+                table: "Profissional",
+                fixedLength: true,
+                maxLength: 2,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uf",
+                table: "Profissional");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Estado",
+                table: "Profissional",
+                type: "char(2) CHARACTER SET utf8mb4",
+                fixedLength: true,
+                maxLength: 2,
+                nullable: true);
+        }
+    }
+}
